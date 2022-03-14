c0c9f4c49da81376a36430a539b3425e7ea2aef5
Voleocage <voleocodes@gmail.com>
2022-03-14 03:53:12 +0000

    Add task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * Description: prints a string
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
+
