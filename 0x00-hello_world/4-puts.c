b97822543adf5292c877c4c1810a27546fbdbe59
Voleocage <voleocodes@gmail.com>
2022-03-14 03:48:30 +0000

    Update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- *function_name = puts
- *Description: used to print out a string
- *Return: Returns a Success
+ *main - function
+ *Description: used to print out "\"Programming is like building a multilingual puzzle"
+ *Return: Returns a Success/ 0
  */
 
 int main(void)
