811defde9142a2efe00b5005ccb079201667b051
Voleocage <voleocodes@gmail.com>
2022-03-14 03:49:10 +0000

    Update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 /**
  *main - function
- *Description: used to print out "\"Programming is like building a multilingual puzzle"
+ *Description: used to print out a string
  *Return: Returns a Success/ 0
  */
 
