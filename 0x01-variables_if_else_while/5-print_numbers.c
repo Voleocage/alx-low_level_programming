aae81a86547f0fa26bde237bb40af0b2218bd523
Voleocage <voleocodes@gmail.com>
2022-03-16 19:30:11 +0000

    Update

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 /**
- *
- *
+ * main - First function
+ * Description: print all single digit numbers of base 10
+ * Return: returns a success 0
  */
 
 int main(void)
@@ -10,9 +11,7 @@ int main(void)
 	int i;
 
 	for (i = 0; i < 10; i++)
-	{
 		printf("%d", i);
-	}
 
 	return (0);
 }
