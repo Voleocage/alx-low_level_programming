03aa1fe5091bee6878d905e7006926895923eb16
Voleocage <voleocodes@gmail.com>
2022-03-16 17:30:54 +0000

    Update

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,12 +11,12 @@ int main(void)
 {
 	int i;
 
-	for (i = 'A'; i <= 'Z'; i++)
-		putchar(i);
-
 	for (i = 'a'; i <= 'z'; i++)
 		putchar(i);
 
-	putchar("\n");
+	for (i = 'A'; i <= 'Z'; i++)
+		putchar(i);
+
+	putchar('\n');
 	return (0);
 }
